8f637e1dbc31baa22e25b53bea58145e939662d6
yuelong.chen <yuelong.chen@yahoo.com>
2018-04-17 10:13:19 +0800

    tensorflow相关方法包装

---
+++ src/learn_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @PROJECT : jupyter_Projects
+# @Time    : 2018/4/17 10:09
+# @Author  : Chen Yuelong
+# @Mail    : yuelong.chen@oumeng.com.cn
+# @File    : learn_utils.py
+# @Software: PyCharm
+
+from __future__ import absolute_import, unicode_literals
+import sys, os
+import tensorflow as tf
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
+
+
+
+
+
+
+def main():
+    '''
+    测试流程
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    main()
